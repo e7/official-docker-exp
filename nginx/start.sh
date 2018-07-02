a412dfff26df9547a5bc0173c87cb7b803893a36
ryuuzaki <ryuuzaki.uchiha@gmail.com>
2018-07-02 17:42:57 +0800

    更新nginx容器配置

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 workdir=$(readlink -f "${0}" | xargs dirname)
-/usr/bin/docker run -d --rm --name webproxy --net host -v "${workdir}/conf.d:/etc/nginx/conf.d" -v /var/log/nginx:/var/log/nginx -v "${workdir}/ssl:/ssl" e7/my-ngx
+/usr/bin/docker run -d --name webproxy --net host -v "${workdir}/conf.d:/etc/nginx/conf.d" -v /var/log/nginx:/var/log/nginx e7/my-ngx
